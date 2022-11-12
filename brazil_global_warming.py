8914cfef024789401f04cbdb977cca4371a07c38
ramarh1 <88624677+ramarh1@users.noreply.github.com>
2022-11-11 23:24:32 -0500

    update

--- brazil_global_warming.py
+++ brazil_global_warming.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-#import plotly.express as px
 
 st.title('Global Warming Trends in Brazil')
 data = pd.read_csv("archive/ENVIRON_DATA.csv")
