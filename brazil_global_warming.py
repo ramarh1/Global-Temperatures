51903726a467a96d7ad8ef994b305b432566c02b
ramarh1 <88624677+ramarh1@users.noreply.github.com>
2022-11-14 03:31:53 -0500

    update mods

--- brazil_global_warming.py
+++ brazil_global_warming.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from itertools import cycle
-from plotly import objs as go
+import plotly.graph_objs as go
 
 st.set_page_config(
     page_title="Environment Dashboard",
